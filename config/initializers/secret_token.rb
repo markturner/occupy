fbe04ac631187191d6e0a207ad569b9ae4b2d489
Mark Turner <markjturner@gmail.com>
2011-11-20 12:28:23 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Occupy::Application.config.secret_token = 'ad5c268f66479ad39461d824a074ebd8208df55553703679f773a69b7aae0e4a40b1dc04c8cee988bc2a925f2430a77224c0748b32e0f17459deb9e03753a319'
